8a48101f650979a9ae05acfbce170e1e3b3d45e4
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-11-15 21:32:32 -0300

    Buscando as top 5 séries

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -12,6 +12,8 @@ public interface SerieRepository extends JpaRepository<Serie, Long>{
 	Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
 
 	List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
+
+	List<Serie> findTop5ByOrderByAvaliacaoDesc();
 	
 	
 }
